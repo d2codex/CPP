e13b6752db2f036bc62cad084a645d9cccbd51cd
d2 <d2.de.sousa@gmail.com>
2026-01-06 22:55:48 +0100

    refactor(main): handle EOF with exceptions and simplify control flow

--- CPP00/ex01/src/main.cpp
+++ CPP00/ex01/src/main.cpp
@@ -1,60 +1,62 @@
 #include "contact.hpp"
 #include "phonebook.hpp"
 #include "colors.hpp"
+#include "utils.hpp"
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 static bool confirmExit()
 {
-	std::string input;
-		
-	std::cout << CYN "Exiting will delete all contacts in the phonebook.\n"
-			  << RESET;
-	std::cout << "Are you sure you want to exit?\n";
-	std::cout << "Type YES to exit and lose all information forever.\n";
-	std::cout << "Type NO to return to the prompt.\n";
+	const std::string message =
+		MAG "Exiting will delete all contacts in the phonebook.\n" RESET
+		"Are you sure you want to exit?\n"
+		"Type YES to exit and lose all information forever.\n"
+		"Type NO to return to the prompt.\n";
 
-	while (true)
-	{
-		std::cout << YEL "> " << RESET;
+	return (promptYesNo(message));
+}
 
-		if (!std::getline(std::cin, input))
-			return (true); // EOF -> exit safely
-		if (input == "YES" || input == "yes")
-			return (true);
-		if (input == "NO" || input == "no")
-			return (false);
-
-		std::cout << "Invalid input.  Please type YES or NO.\n";
-	}
+bool PhoneBook::isEmpty() const
+{
+	return (_totalContacts == 0);
 }
 
 int	main(void)
 {
 	PhoneBook phonebook;
 	std::string command;
-
-	while (true)
+	try
 	{
-		std::cout << YEL << "    === Welcome to the PhoneBook ===\n" << RESET;
-		std::cout << "Type ADD to add a contact to the phonebook\n";
-		std::cout << "Type SEARCH to display the phonebook\n";
-		std::cout << "Type EXIT to exit the program\n";
-		std::cout << YEL << "> " << RESET;
-		if (!std::getline(std::cin, command))
-			break ;
-		if (command == "ADD" || command == "add")
-			phonebook.addContact();
-	//	else if (command == "SEARCH" || command == "search")
-	//		phonebook.searchContacts();
-		else if (command == "EXIT" || command == "exit")
+		while (true)
 		{
-			if (confirmExit())
-				break ;
+			std::cout << YEL << "    === Welcome to the PhoneBook ===\n" << RESET;
+			std::cout << "Type ADD to add a contact to the phonebook\n";
+			std::cout << "Type SEARCH to display the phonebook\n";
+			std::cout << "Type EXIT to exit the program\n";
+			std::cout << YEL << "> " << RESET;
+			if (!std::getline(std::cin, command))
+				throw std::runtime_error("EOF");
+			toUpperCase(command);
+			if (command == "ADD")
+				phonebook.addContact();
+			else if (command == "SEARCH")
+				phonebook.searchContacts();
+			else if (command == "EXIT")
+			{
+				if (phonebook.isEmpty())
+					break ;
+				if (confirmExit())
+					break ;
+			}
+			else
+				std::cout << RED << "Invalid command. Please try again.\n" << RESET;
+			std::cout << "\n";
 		}
-		else
-			std::cout << RED << "Invalid command. Please try again.\n" << RESET;
-		std::cout << "\n";
+	}
+	catch (const std::runtime_error& e)
+	{
+		std::cout << "\n" << BLU << "Input stream closed.\n" << RESET;
 	}
 	std::cout << BLU << "Goodbye and have a nice day.\n" << RESET;
 	return (0);
