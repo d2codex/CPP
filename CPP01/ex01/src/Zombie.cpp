87f626b0de9a5727a3779897e0aa4a64977e7c4c
d2 <d2.de.sousa@gmail.com>
2026-01-13 17:47:53 +0100

    refactor(Zombie): move zombieHorde() implementation to zombieHorde.cpp

--- CPP01/ex01/src/Zombie.cpp
+++ CPP01/ex01/src/Zombie.cpp
@@ -63,33 +63,3 @@ void	Zombie::setZombieName(const std::string& name)
 	_name = name;
 }
 
-Zombie* zombieHorde(int N, std::string name)
-{
-	if (N <= 0)
-	{
-		std::cerr << RED "Error. A horde must contain at least 1 zombie\n" RESET;
-		return (NULL);
-	}
-	if (N > MAX_HORDE_SIZE)
-	{
-		std::cerr << RED "Error. Horde size exceeds max limit (1000000)\n" RESET;
-		return (NULL);
-	}
-
-	Zombie *horde = NULL;
-	try
-	{
-		horde = new Zombie[N];
-	}
-	catch (std::bad_alloc& e)
-	{
-		std::cerr << RED "Error: memory allocation failed for " << N
-				  << " zombies (" << e.what() << ")\n" RESET;
-		return (NULL);
-	}
-
-	for(int i = 0; i < N; i++)
-		horde[i].setZombieName(name);
-
-	return (horde);
-}
