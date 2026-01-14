d14a2cab31bc3ccfe3800a621c1d5bb7add78445
d2 <d2.de.sousa@gmail.com>
2026-01-14 14:14:16 +0100

    Initial commit: Demonstrate pointer and reference usage with string
    - Prints memory addresses of string, pointer, and reference
    - Prints values of string, pointer, and reference
    - Aligns output using std::setw and std::left
    - Adds color coding for better readability
    - Includes comments explaining pointers vs references

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include "../colors.hpp"
+
+#define LABLE_WIDTH 34
+
+int	main(void)
+{
+	// original string
+	const std::string str = "HI THIS IS BRAIN";
+	// a pointer stores an address
+	const std::string* stringPTR = &str;
+	// this is just an alias of the original string
+	const std::string& stringREF = str;
+
+	// original string address
+	std::cout << WHT
+			  << std::left << std::setw(LABLE_WIDTH)
+			  << "Memory address of str:"
+			  << &str << RESET << '\n';
+
+	// to get the address stored inside the pointer
+	// just print the value of the ptr
+	std::cout << BLU
+			  << std::left << std::setw(LABLE_WIDTH)
+			  << "Memory address held by stringPTR: "
+			  << stringPTR << RESET <<'\n';
+	
+	// the pointer itself has its own address
+	// to access it we do &stringPTR (should be diff from stored address).
+	std::cout << CYN
+			  << std::left << std::setw(LABLE_WIDTH)
+			  << "Memory addr of actual stringPTR: " << &stringPTR
+			  << RESET << '\n';
+
+	// treat it just like the original string use &to get the address
+	// of the reference, which is the same as the original str
+	std::cout << YEL
+			  << std::left << std::setw(LABLE_WIDTH)
+			  << "Memory address held by stringREF: " << &stringREF
+			  << RESET << '\n';
+	
+	//original value of string
+	std::cout << WHT
+			  << std::left << std::setw(LABLE_WIDTH)
+			  << "Value of str: " << str
+			  << RESET << '\n';
+
+	// to get the value of the stored address we must dereference the ptr
+	std::cout << BLU 
+			  << std::left << std::setw(LABLE_WIDTH)
+			  << "Value pointed to by stringPTR: " << *stringPTR
+			  << RESET << '\n';
+	
+	// same here: a reference is an alias, so we acccess the value directly
+	std::cout << YEL
+			  << std::left << std::setw(LABLE_WIDTH)
+			  << "Value pointed to by stringREF: " << stringREF
+			  << RESET << '\n';
+}
