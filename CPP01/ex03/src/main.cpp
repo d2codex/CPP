2e9c573e87c42e9e136c2646b389dd51a907068a
d2 <d2.de.sousa@gmail.com>
2026-01-16 10:14:09 +0100

    refactor: remove temporary unit tests from src/main.cpp
    - Clean up src/main.cpp to only contain the subject-provided test

--- CPP01/ex03/src/main.cpp
+++ CPP01/ex03/src/main.cpp
@@ -3,41 +3,13 @@
 #include "colors.hpp"
 #include <iostream>
 
-struct WeaponTest
-{
-	std::string description;
-	std::string typeValue;
-};
+
 
 int	main(void)
 {
-	WeaponTest test[] = {
-		{"Valid Normal case", "club"},
-		{"Invalid Empty type", ""},
-		{"Invalid White spaces only", "		 "},
-		{"Valid complex type", "Crude Spiked Club"},
-		{"Valid special characters", "afkl;j1234#$@"}
-	};
-	size_t numTests = sizeof(test) / sizeof(test[0]);
-	for (size_t i = 0; i < numTests; i++)
-	{
-		std::cout << YEL "Test[" << i << "] " << test[i].description
-			<< RESET <<'\n';
-
-		try
-		{
-			Weapon weapon(test[i].typeValue);
-			std::cout << "Input:   " << test[i].typeValue << '\n';
-			std::cout << "getType: " << weapon.getType() << '\n';
-			if (weapon.getType() == test[i].typeValue)
-				std::cout << GRN "Success\n" RESET;
-			else
-				std::cout << RED "Fail: type mismatch\n";
-		}
-		catch (const std::exception& e)
-		{
-			std::cerr << RED "Error: " << e.what() << RESET << '\n';
-		}
-	}
-	return (0);
+	Weapon club = Weapon("crude spiked club");
+	HumanA bob("Bob", club);
+	bob.attack();
+	club.setType("some other type of club");
+	bob.attack();
 }
