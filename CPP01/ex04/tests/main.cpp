ab78120d4c165108d84ef94a352c098fbe395aff
d2 <d2.de.sousa@gmail.com>
2026-01-18 15:26:38 +0100

    test(cpp01/ex04): add unit tests for validateArgs

---
+++ CPP01/ex04/tests/main.cpp
@@ -0,0 +1,25 @@
+#include "tests.hpp"
+#include "colors.hpp"
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	
+    std::cout << YEL << "=======================================\n"
+					 << "         Running all unit tests\n"
+					 << "=======================================\n";
+	
+	int	totalFailures = 0;
+	int	totalTestSuites = 0;
+
+	totalFailures += validateArgsTest();
+	totalTestSuites++;
+
+	std::cout << YEL "============= Test summary ============" RESET << '\n'
+			  << WHT "Total test suits: " << totalTestSuites << RESET << '\n'
+			  << RED "total failures: " << totalFailures << RESET << '\n';
+	std::cout << YEL "=======================================\n" RESET;
+	if (totalFailures == 0)
+		std::cout << GRN "ALL TESTS PASSED!\n" RESET;
+}
