98138da7d973a6629313d4a02fa46b6be785201a
d2 <d2.de.sousa@gmail.com>
2026-01-19 09:19:34 +0100

    docs(cpp01/ex05) add doxygen comments and 42 header to all files

--- CPP01/ex05/includes/Harl.hpp
+++ CPP01/ex05/includes/Harl.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diade-so <diade-so@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/19 09:18:18 by diade-so          #+#    #+#             */
+/*   Updated: 2026/01/19 09:18:38 by diade-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HARL_HPP
 #define HARL_HPP
 
@@ -9,6 +21,13 @@
 
 #include <string>
 
+/**
+ * @class Harl
+ * @brief Simple logger that prints complaints at different levels.
+ *
+ * The class maps a string level to a corresponding message function.
+ * Supported levels: DEBUG, INFO, WARNING, ERROR.
+ */
 class Harl
 {
 public:
--- CPP01/ex05/src/Harl.cpp
+++ CPP01/ex05/src/Harl.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diade-so <diade-so@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/19 09:18:13 by diade-so          #+#    #+#             */
+/*   Updated: 2026/01/19 09:18:15 by diade-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 #include "colors.hpp"
 #include <iostream>
@@ -6,30 +18,60 @@
 
 #define COL_WIDTH 9
 
+/**
+ * @brief Print a debug-level message.
+ *
+ * Outputs a message describing extra detail or low-priority information.
+ */
 void	Harl::debug(void)
 {
 	std::cout << YEL << std::setw(COL_WIDTH) << "[DEBUG]" << RESET
 			  << " I love having extra bacon for my burger. I really do!\n";
 }
 
+
+/**
+ * @brief Print an info-level message.
+ *
+ * Outputs a general informational message about a minor issue.
+ */
 void	Harl::info(void)
 {
 	std::cout << BLU << std::setw(COL_WIDTH) << "[INFO]" << RESET
 			  << " You didn’t put enough bacon in my burger!\n";
 }
 
+/**
+ * @brief Print a warning-level message.
+ *
+ * Outputs a message indicating a potential problem or complaint.
+ */
 void	Harl::warning(void)
 {
 	std::cout << MAG << std::setw(COL_WIDTH) << "[WARNING]" << RESET 
 			  << " I think I deserve to have some extra bacon for free.\n";
 }
 
+/**
+ * @brief Print an error-level message.
+ *
+ * Outputs a critical message indicating an unacceptable situation.
+ */
 void	Harl::error(void)
 {
 	std::cout << RED << std::setw(COL_WIDTH) << "[ERROR]" << RESET
 			  << " This is unacceptable! I want to speak to the manager now. \n";
 }
 
+/**
+ * @brief Dispatch a complaint based on the given level.
+ *
+ * Matches the input level against known levels and calls the
+ * corresponding member function. Prints a message if no match
+ * is found.
+ *
+ * @param level Complaint severity ("DEBUG", "INFO", "WARNING", "ERROR").
+ */
 void	Harl::complain(std::string level)
 {
 	// we create an array of pointers *functions[]
