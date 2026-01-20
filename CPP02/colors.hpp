41a69c419fe81212966803146918b14b99a22578
d2 <d2.de.sousa@gmail.com>
2026-01-20 23:04:38 +0100

    feat(cpp02): add colors header

---
+++ CPP02/colors.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diade-so <diade-so@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/06 23:43:16 by diade-so          #+#    #+#             */
+/*   Updated: 2026/01/12 13:59:47 by diade-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_HPP
+#define COLORS_HPP
+
+// Basic ANSI Colors
+# define BLK "\033[30m"
+# define RED "\033[31m"
+# define GRN "\033[32m"
+# define YEL "\033[33m"
+# define BLU "\033[34m"
+# define MAG "\033[35m"
+# define CYN "\033[36m"
+# define WHT "\033[37m"
+# define RESET "\033[0m"
+
+// Bright ANSI Colors
+# define BR_BLK "\033[1;30m"
+# define BR_RED "\033[1;31m"
+# define BR_GRN "\033[1;32m"
+# define BR_YEL "\033[1;33m"
+# define BR_BLU "\033[1;34m"
+# define BR_MAG "\033[1;35m"
+# define BR_CYN "\033[1;36m"
+# define BR_WHT "\033[1;37m"
+
+# endif
