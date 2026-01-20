18b77a50950cab714ee58cfe67193c766cb28e8e
d2 <d2.de.sousa@gmail.com>
2026-01-20 17:09:51 +0100

    feat(cpp02/ex00) implement main entry point for fixed

---
+++ CPP02/ex00/src/main.cpp
@@ -0,0 +1,31 @@
+#include "Fixed.hpp"
+#include "colors.hpp"
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << CYN "Usage: ./bin/Fixed <log mode>\n" RESET
+				  << "Log modes: <debug> <info> <warning> <error> <none>\n";
+		return (1);
+	}
+	Logger::logLevel level = Logger::stringToLevel(argv[1]);
+	if (level == Logger::INVALID)
+	{
+		std::cout << RED "Error: Invalid log level\n" RESET;
+		return (1);
+	}
+	Logger::setThreshold(level);
+
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+}
