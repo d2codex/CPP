2844ba349a9aa5b31122f0ec65cdbf8023e704db
d2 <d2.de.sousa@gmail.com>
2026-01-30 23:42:46 +0100

    fix(cpp02/03): correct Point getter return types to const Fixed&

--- cpp02/ex03/includes/Point.hpp
+++ cpp02/ex03/includes/Point.hpp
@@ -7,20 +7,19 @@ class Point
 public:
 	Point();
 	Point(const float x, const float y);
+	Point(const Point& other);
 	// required by subject, but unimplemented to prevent assignment
 	// cannot be used because _x and _y are constant memebers
 	// also this should be in the private sector
 	Point& operator=(const Point&);
-	Point(const Point& other);
 	~Point();
 
-	const getX() const;
-	const getY() const;
+	const Fixed& getX() const;
+	const Fixed& getY() const;
 
 private:
 	Fixed const	_x;
 	Fixed const	_y;
-
-}
+};
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point);
