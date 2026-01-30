312a9d5f32b1dfe33f7d41a069628793f506dca9
d2 <d2.de.sousa@gmail.com>
2026-01-30 15:37:22 +0100

    fix(cpp02/03) changed const Fixed x/y to Fixed const x/y as per the subject

--- cpp02/ex03/includes/Point.hpp
+++ cpp02/ex03/includes/Point.hpp
@@ -7,19 +7,20 @@ class Point
 public:
 	Point();
 	Point(const float x, const float y);
+	// required by subject, but unimplemented to prevent assignment
+	// cannot be used because _x and _y are constant memebers
+	// also this should be in the private sector
 	Point& operator=(const Point&);
+	Point(const Point& other);
 	~Point();
 
 	const getX() const;
 	const getY() const;
 
 private:
-	const Fixed	_x;
-	const Fixed	_y;
+	Fixed const	_x;
+	Fixed const	_y;
 
-	// required by subject, but unimplemented to prevent assignment
-	// cannot be used because _x and _y are constant memebers
-	Point(const Point& other);
 }
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point);
