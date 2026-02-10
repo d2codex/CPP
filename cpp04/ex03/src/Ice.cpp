8b566fe0d12c73470a788b1abb6d839feb83f35a
d2 <d2.de.sousa@gmail.com>
2026-02-10 20:28:19 +0100

    fix(cpp04/03) use correct object name

--- cpp04/ex03/src/Ice.cpp
+++ cpp04/ex03/src/Ice.cpp
@@ -45,5 +45,5 @@ AMateria* Ice::clone() const
 
 void	Ice::use(ICharacter& target)
 {
-	std::cout << "* shoots an ice bolt at " << target << '\n';
+	std::cout << "* shoots an ice bolt at " << target.getName() << " *\n";
 }
