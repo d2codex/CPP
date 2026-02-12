f58343972c6df1d71daf4fd015fece626b322b88
d2 <d2.de.sousa@gmail.com>
2026-02-13 00:37:17 +0100

    tests(cpp05/01) add test framework from prev exercise

---
+++ cpp05/ex01/includes/tests.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "colors.hpp"
+
+void	printHeader(const std::string& testName);
+void	printSummary(int failed, int total);
+
+/* helper for C-strings */
+inline bool equalHelper(const char* a, const char* b)
+{
+	return (std::strcmp(a, b) == 0);
+}
+
+/* mixed string types */
+inline bool equalHelper(const char* a, const std::string& b)
+{
+	return b == a;
+}
+
+inline bool equalHelper(const std::string& a, const char* b)
+{
+	return a == b;
+}
+
+/* generic helper for all other types */
+template<typename T>
+bool equalHelper(const T& a, const T& b)
+{
+	return (a == b);
+}
+
+/* template assertEqual using equalHelper */
+template<typename T1, typename T2>
+bool	assertEqual(const char* testName, const T1& expected,
+					const T2& actual)
+{
+	if (equalHelper(expected, actual))
+	{
+		std::cout << grn("PASS ") << testName << '\n';
+		return (true);
+	}
+	std::cout << red("FAIL ") << testName << '\n'
+			  << "expected: " << expected << '\n'
+			  << "actual:   " << actual << '\n';
+	return (false);
+}
+
+/* template assertNotEqual using equalHelper */
+template<typename T1, typename T2>
+bool	assertNotEqual(const char* testName, const T1& expected,
+					const T2& actual)
+{
+	if (!equalHelper(expected, actual))
+	{
+		std::cout << grn("PASS ") << testName << '\n';
+		return (true);
+	}
+	std::cout << red("FAIL ") << testName << '\n'
+			  << mag("results must be different") << '\n'
+			  << "expected: " << expected << '\n'
+			  << "actual:   " << actual << '\n';
+	return (false);
+}
+
+int		testBureaucrat();
+int		testForm();
---
+++ cpp05/ex01/tests/testBureaucrat.cpp
@@ -0,0 +1,186 @@
+#include "Bureaucrat.hpp"
+#include "tests.hpp"
+#include "Logger.hpp"
+
+int	testBureaucrat()
+{
+	printHeader("Bureaucrat Class");
+	int	failed = 0;
+	int	total = 0;
+
+	// Class test: Exception too high returns correct message
+	{
+		std::exception* e = new Bureaucrat::GradeTooHighException();
+		const char* expected = "Bureaucrat grade too high";
+
+		total++;
+		if (!assertEqual("exception too high returns correct message",
+						 expected, e->what()))
+			failed++;
+		delete e;
+	}
+	// Class test: Exception too low returns correct message
+	{
+		std::exception* e = new Bureaucrat::GradeTooLowException();
+		const char* expected = "Bureaucrat grade too low";
+
+		total++;
+		if (!assertEqual("exception too low returns correct message",
+						 expected, e->what()))
+			failed++;
+		delete e;
+	}
+	// object: grade too high - out of range
+	{
+		try
+		{
+			Bureaucrat("plankton", 0);
+		}
+		catch (Bureaucrat::GradeTooHighException& e)
+		{
+			const char* expected = "Bureaucrat grade too high";
+
+			total++;
+			if (!assertEqual("grade too high - out of range", expected, e.what()))
+				failed++;
+		}
+	}
+	// object: grade too low - out of range
+	{
+		try
+		{
+			Bureaucrat("plankton", 151);
+		}
+		catch (Bureaucrat::GradeTooLowException& e)
+		{
+			const char* expected = "Bureaucrat grade too low";
+
+			total++;
+			if (!assertEqual("grade too low - out of range", expected, e.what()))
+				failed++;
+		}
+	}
+	// getGrade normal valid value
+	{
+		Bureaucrat a("plankton", 70);
+
+		total++;
+		if (!assertEqual("getGrade normal valid value", 70, a.getGrade()))
+			failed++;
+	}
+	// getName valid name
+	{
+		Bureaucrat a("plankton", 70);
+		
+		total++;
+		if (!assertEqual("getName valid name", "plankton", a.getName()))
+			failed++;
+	}
+	// getName invalid empty name
+	{
+		try
+		{
+			Bureaucrat a("", 100);
+		}
+		catch (std::exception& e)
+		{
+			const char* expected = "Bureaucrat name cannot be empty";
+			if (!assertEqual("getName invalid empty name", expected, e.what()))
+				failed++;
+		}
+	}
+	// increment valid normal value
+	{
+		Bureaucrat a("plankton", 70);
+		a.incrementGrade();	
+		total++;
+		if (!assertEqual("incrementGrade - valid normal value", 69, a.getGrade()))
+			failed++;
+	}
+	// increment edge case - grade at max value
+	{
+		try
+		{
+			Bureaucrat a("plankton", 1);
+			a.incrementGrade();	
+		}
+		catch (Bureaucrat::GradeTooHighException& e)
+		{
+			const char* expected = "Bureaucrat grade too high";
+			total++;
+			if (!assertEqual("incrementGrade - invalid grade at max value",
+							 expected, e.what()))
+				failed++;
+		}
+	}
+	// decrement valid normal value
+	{
+		Bureaucrat a("plankton", 70);
+		a.decrementGrade();
+		total++;
+		if (!assertEqual("decrementGrade - valid normal value", 71, a.getGrade()))
+			failed++;
+	}
+	// decrement edge case - grade at min value
+	{
+		try
+		{
+			Bureaucrat a("plankton", 150);
+			a.decrementGrade();
+		}
+		catch (Bureaucrat::GradeTooLowException& e)
+		{
+			const char* expected = "Bureaucrat grade too low";
+			total++;
+			if (!assertEqual("decrementGrade - invalid grade at min value",
+							 expected, e.what()))
+				failed++;
+		}
+	}
+	// operator overload
+	{
+		Bureaucrat a("spongebob", 32);
+
+		std::ostringstream oss;
+		oss << a;
+		std::string expected = "spongebob, bureaucrat grade 32";
+		
+		total++;
+		if (!assertEqual("operator overload prints correct message",
+						 expected, oss.str()))
+			failed++;
+	}
+	// Copy constructor
+	{
+		Bureaucrat original("Sandy", 42);
+		Bureaucrat copy(original);
+
+		total++;
+		if (!assertEqual("copy constructor copies grade", original.getGrade(),
+					copy.getGrade()))
+			failed++;
+		total++;
+		if (!assertEqual("copy constructor copies name", original.getName(),
+					copy.getName()))
+			failed++;
+	}
+	// Copy assignment
+	{
+		Bureaucrat a("Patrick", 100);
+		Bureaucrat b("Squidward", 50);
+
+		b = a; // assign Patrick to Squidward
+
+		total++;
+		if (!assertEqual("copy assignment copies grade", a.getGrade(),
+					b.getGrade()))
+			failed++;
+		total++;
+		if (!assertEqual("copy assignment keeps original name",
+					std::string("Squidward"), b.getName()))
+			failed++;
+	}
+
+	printSummary(failed, total);
+	return (failed);
+}
