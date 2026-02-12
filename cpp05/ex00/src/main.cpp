bf274f9e7366bb10b291e7827b2adea175292b68
d2 <d2.de.sousa@gmail.com>
2026-02-12 10:43:27 +0100

    feat(cpp05/00) add logger and main

---
+++ cpp05/ex00/src/main.cpp
@@ -0,0 +1,50 @@
+#include "Logger.hpp"
+#include "colors.hpp"
+#include <iostream>
+
+bool initLogger(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		Logger::get().setThreshold(NONE);
+		return (true);
+	}
+	if (argc == 2)
+	{
+		logLevel level = Logger::stringToLevel(argv[1]);
+		if (level == INVALID)
+		{
+			std::cout << red("Error. invalid log level\n");
+			std::cout << cyn("usage: ./bin/<program_name> [log level]\n");
+			std::cout << cyn("log levels: info warning error\n");
+			return (false);
+		}
+		#ifndef DBUG
+		if (level == DEBUG)
+		{
+			std::cout << mag("Warning: DEBUG messages are not enabled in this build.\n");
+			Logger::get().setThreshold(INFO); // fallback threshold
+			return (true);
+		}
+		#endif
+		Logger::get().setThreshold(level);
+		return (true);
+	}
+	std::cout << cyn("usage: ./bin/<program_name> [log level]\n");
+	std::cout << cyn("log levels: info warning error\n");
+	return (false);
+}
+
+int	main(int argc, char **argv)
+{
+	if (!initLogger(argc, argv))
+		return (1);
+
+	std::cout << "test logger\n";
+
+	LOG_DEBUG() << "debug works";
+	LOG_INFO() << "info works";
+	LOG_WARNING() << "warning works";
+	LOG_ERROR() << "error works";
+
+}
