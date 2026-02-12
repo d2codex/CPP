6c0afec260283044cab06c3ed3e190e9cde354da
d2 <d2.de.sousa@gmail.com>
2026-02-13 00:38:06 +0100

    feat(cpp05/04) add main

---
+++ cpp05/ex01/src/main.cpp
@@ -0,0 +1,53 @@
+#include "Form.hpp"
+#include "Logger.hpp"
+#include "colors.hpp"
+#include <iostream>
+
+bool initLogger(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		Logger::get().setThreshold(ERROR);
+		return (true);
+	}
+	if (argc == 2)
+	{
+		logLevel level = Logger::stringToLevel(argv[1]);
+		if (level == INVALID)
+		{
+			std::cout << red("Error. invalid log level\n");
+			std::cout << cyn("usage: ./bin/<program_name> [log level]\n");
+			std::cout << cyn("log levels: debug(debug build only) info warning\n");
+			return (false);
+		}
+		#ifndef DBUG
+		if (level == DEBUG)
+		{
+			std::cout << mag("Warning: DEBUG messages are not enabled in this build.\n");
+			Logger::get().setThreshold(INFO); // fallback threshold
+			return (true);
+		}
+		#endif
+		Logger::get().setThreshold(level);
+		return (true);
+	}
+	std::cout << cyn("usage: ./bin/<program_name> [log level]\n");
+	std::cout << cyn("log levels: debug(debug build only) info warning\n");
+	return (false);
+}
+
+void printTestName(const std::string& testName)
+{
+	std::cout << yel("======================================================\n");
+	std::cout << YEL << "Testing " << testName << RESET << '\n';
+}
+
+
+int	main(int argc, char **argv)
+{
+	if (!initLogger(argc, argv))
+		return (1);
+
+	Form a("W-2", 75, 88);
+
+}
