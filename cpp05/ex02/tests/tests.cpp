982455a5f2c721617b20c18aae07cb993667ac6d
d2 <d2.de.sousa@gmail.com>
2026-02-16 01:07:08 +0100

    tests(cpp05/02) add fileExists and fileDoesNot exist to tests.hpp

--- cpp05/ex02/tests/tests.cpp
+++ cpp05/ex02/tests/tests.cpp
@@ -2,6 +2,8 @@
 #include "colors.hpp"
 #include <iostream>
 #include <cstring>
+#include <fstream>
+#include <string>
 
 void printHeader(const std::string& testName)
 {
@@ -20,4 +22,32 @@ void printSummary(int failed, int total)
         std::cout << grn("All tests passed!\n");
 	}
 	std::cout << yel("=======================================\n");
+}
+
+bool fileExists(const std::string& testName, const std::string& filename)
+{
+    std::ifstream f(filename.c_str());
+	if (f.good())
+	{
+		std::cout << grn("PASS ") << testName << '\n';
+		return (true);
+	}
+	std::cout << red("FAIL ") << testName << '\n'
+			  << "file not found" << '\n';
+	return (false);
+}
+
+bool fileDoesNotExist(const std::string& testName, const std::string& filename)
+{
+    std::ifstream f(filename.c_str());
+    if (!f.good())
+    {
+        std::cout << grn("PASS ") << testName << '\n';
+        return true;
+    }
+    else
+    {
+        std::cout << red("FAIL ") << testName << '\n';
+        return false;
+    }
 }
