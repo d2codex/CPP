aebcd75f3599cf8dd6734380581e64ac4730129c
d2 <d2.de.sousa@gmail.com>
2026-02-20 11:00:04 +0100

    feat(cpp06/00) add testing framework

---
+++ cpp06/ex00/tests/mainTest.cpp
@@ -0,0 +1,39 @@
+#include "tests.hpp"
+#include "Logger.hpp"
+#include "colors.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	logLevel level = ERROR;
+
+	Logger::get().setThreshold(level);
+    std::cout << yel("=======================================\n"
+					 "         Running all unit tests\n"
+					 "=======================================\n");
+	
+	int	totalFailures = 0;
+	int	totalTestSuites = 0;
+
+/*	totalTestSuites++;
+	totalFailures += testBureaucrat();
+
+	totalTestSuites++;
+	totalFailures += testShrubberyCreationForm();
+
+	totalTestSuites++;
+	totalFailures += testRobotomyRequestForm();
+
+	totalTestSuites++;
+	totalFailures += testPresidentialPardonForm();
+
+	totalTestSuites++;
+	totalFailures += testIntern();
+*/
+	std::cout << blu("Test Suites Summary\n")
+			  << wht("Total test suits: ") << totalTestSuites << '\n'
+			  << red("total failures: ") << totalFailures << '\n'
+			  << yel("=======================================\n");
+	if (totalFailures == 0)
+		std::cout << grn("ALL TESTS PASSED!\n");
+}
