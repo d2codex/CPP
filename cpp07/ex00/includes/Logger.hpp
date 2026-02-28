6d3016501dbb1080ef8c64612572639fd62adc56
d2 <d2.de.sousa@gmail.com>
2026-02-28 22:58:15 +0100

    feat(cpp07/00) include logger and colors

---
+++ cpp07/ex00/includes/Logger.hpp
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Logger.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diade-so <diade-so@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 16:03:12 by diade-so          #+#    #+#             */
+/*   Updated: 2026/02/24 16:03:14 by diade-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "colors.hpp"
+#include <string>
+#include <sstream>
+#include <iostream>
+
+class LogLine;
+
+/**
+ * @class Logger
+ * @brief Singleton logger supporting multiple log levels and RAII.
+ *
+ * Logger manages a single logging instance, allowing messages to be
+ * streamed via LogLine objects. It supports thresholds to filter
+ * messages, converting string levels to enum, and flushes messages
+ * to stdout with optional color-coded prefixes.
+ */
+class Logger
+{
+public:
+	/* Supported log levels */
+	enum logLevel
+	{
+		INVALID = -1,
+		NONE = 0,
+		DEBUG = 1,
+		INFO = 2,
+		WARNING = 3,
+		ERROR = 4,
+	};
+
+	Logger();
+	Logger(const Logger& other);
+	Logger& operator=(const Logger&);
+	~Logger();
+	
+	/* Singleton instance */
+	static		Logger& get();
+	
+	/* Flush a completed message */
+	void		flush(const std::string& prefix,
+					  const std::string& message,
+					  logLevel level);
+
+	/* Set the current log threshold */
+	void		setThreshold(logLevel level);
+	logLevel	threshold() const;
+
+	/* RAII factory for LogLine */
+	LogLine		makeLine(const std::string& prefix, logLevel level);
+
+	/* Convert string to logLevel enum */
+	static		logLevel stringToLevel(const std::string& levelStr);
+
+private:
+	logLevel _threshold;
+};
+
+
+/**
+ * @class LogLine
+ * @brief RAII helper to build and flush a log message.
+ *
+ * LogLine accumulates streamed values into an internal buffer. When
+ * destroyed, it automatically flushes the message through the
+ * associated Logger, respecting the log level and threshold.
+ */
+class LogLine
+{
+public:
+	LogLine(Logger& logger, const std::string& prefix, Logger::logLevel level);
+	LogLine(const LogLine& other);
+	LogLine& operator=(const LogLine& other);
+	~LogLine();
+
+	template<typename T>
+	LogLine& operator<<(const T& value)
+	{
+		_stream << value;
+		return (*this);
+	}
+
+private:
+	LogLine();
+	Logger&				_logger;
+	std::ostringstream	_stream;
+	std::string			_prefix;
+	Logger::logLevel			_level;
+};
+
+/* log macros */
+//#ifdef DBUG
+#define LOG_DEBUG()		Logger::get().makeLine(yel("[DEBUG] "), Logger::DEBUG)
+//#else
+//#define LOG_DEBUG()		Logger::get().makeLine(yel(""), Logger::NONE)
+//#endif
+
+#define LOG_INFO()		Logger::get().makeLine(blu("[INFO] "), Logger::INFO)
+#define LOG_WARNING()	Logger::get().makeLine(mag("[WARNING] "), Logger::WARNING)
+#define LOG_ERROR()		Logger::get().makeLine(red("[ERROR] "), Logger::ERROR)
