a9f03fcc7eeff83d39e445dcc6a2df10376ce792
d2 <d2.de.sousa@gmail.com>
2026-03-04 00:15:05 +0100

    tsets(cpp07/02) add unit testing for Subscript Operator []

---
+++ cpp07/ex02/tests/testSubscriptOperator.cpp
@@ -0,0 +1,69 @@
+#include "tests.hpp"
+#include "Array.hpp"
+#include "Logger.hpp"
+
+int testSubscriptOperator()
+{
+	int failed = 0;
+	int	total = 0;
+
+	printHeader("Subscript Operator suite");
+	// non const assignment and out of bounds check
+	{
+		Array<int> a(3);
+		a[0] = 42;
+		a[1] = 24;
+		a[2] = -42;
+
+		if (!assertEqual("size matches", 3, a.size()))
+			failed++;
+		total++;
+
+		if (!assertEqual("index 0 matches", 42, a[0]))
+			failed++;
+		total++;
+
+		if (!assertEqual("index 1 matches", 24, a[1]))
+			failed++;
+		total++;
+
+		if (!assertEqual("index 2 matches", -42, a[2]))
+			failed++;
+		total++;
+
+
+		bool thrown = false;
+		try
+		{
+			a[3]; // should throw
+		}
+		catch (const std::out_of_range& e)
+		{
+			//LOG_ERROR() << e.what();
+			thrown = true;
+		}
+		if (!assertEqual("nconst out of bounds - a[3] should throw", true, thrown))
+			failed++;
+		total++;
+	}
+	// const array
+	{
+		const Array<char> a(3);
+	
+		bool thrown = false;
+		try
+		{
+			a[3]; // should throw
+		}
+		catch (const std::out_of_range& e)
+		{
+			//LOG_ERROR() << e.what();
+			thrown = true;
+		}
+		if (!assertEqual("const out of bounds - a[3] should throw", true, thrown))
+			failed++;
+		total++;
+	}
+	printSummary(failed, total);
+	return (failed);
+}
