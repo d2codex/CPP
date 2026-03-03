aa2ac6f259cedec04c7c315017ab553c299f3403
d2 <d2.de.sousa@gmail.com>
2026-03-03 21:28:07 +0100

    tests(cpp07/02) add testing framework

---
+++ cpp07/ex02/tests/mainTest.cpp
@@ -0,0 +1,31 @@
+#include "tests.hpp"
+#include "Logger.hpp"
+#include "colors.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Logger::logLevel level = Logger::INFO;
+
+	Logger::get().setThreshold(level);
+	LOG_DEBUG() << "test";
+    std::cout << yel("=======================================\n"
+					 "         Running all unit tests\n"
+					 "=======================================\n");
+	
+	int	totalFailures = 0;
+	int	totalTestSuites = 0;
+
+	totalTestSuites++;
+	totalFailures += testDefaultCtor();
+
+	totalTestSuites++;
+	totalFailures += testParamCtor();
+
+		std::cout << blu("Test Suites Summary\n")
+			  << wht("Total test suits: ") << totalTestSuites << '\n'
+			  << red("total failures: ") << totalFailures << '\n'
+			  << yel("=======================================\n");
+	if (totalFailures == 0)
+		std::cout << grn("ALL TESTS PASSED!\n");
+}
