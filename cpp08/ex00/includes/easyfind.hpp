50cf170320d3c0174eef8d985e4a0070ffb5aea1
d2 <d2.de.sousa@gmail.com>
2026-03-06 23:34:18 +0100

    feat(cpp08/00) implement easyfind function template

---
+++ cpp08/ex00/includes/easyfind.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <algorithm>
+#include <stdexcept>
+#include "Logger.hpp"
+
+template<typename T>
+typename T::iterator easyfind(T& container, int value)
+{
+	typename T::iterator it;
+
+	it = std::find(container.begin(), container.end(), value);
+
+	if (it == container.end())
+		throw std::runtime_error("Value not found");
+	
+	LOG_DEBUG() << "Value: " << value << " found at index "
+				<< std::distance(container.begin(), it);
+
+	return (it);
+}
