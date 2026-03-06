4ce15d657cc8da75e2aa26c1eb9e7282db43cf6f
d2 <d2.de.sousa@gmail.com>
2026-03-06 23:34:31 +0100

    tests(cpp08/00) add testing framework

---
+++ cpp08/ex00/src/main.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diade-so <diade-so@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/06 23:17:48 by diade-so          #+#    #+#             */
+/*   Updated: 2026/03/06 23:31:08 by diade-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include "tests.hpp"
+#include "Logger.hpp"
+#include "colors.hpp"
+#include <iostream>
+
+bool initLogger(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		Logger::get().setThreshold(Logger::NONE);
+		return (true);
+	}
+	if (argc == 2)
+	{
+		Logger::logLevel level = Logger::stringToLevel(argv[1]);
+		if (level == Logger::INVALID)
+		{
+			std::cout << red("Error. invalid log level\n");
+			std::cout << cyn("usage: ./bin/<program_name> [log level]\n");
+			std::cout << cyn("log levels: debug info warning error\n");
+			return (false);
+		}
+		Logger::get().setThreshold(level);
+		return (true);
+	}
+	std::cout << cyn("usage: ./bin/<program_name> [log level]\n");
+	std::cout << cyn("log levels: debug info warning error\n");
+	return (false);
+}
+
+
+
+int	main(int argc, char **argv)
+{
+
+	if (!initLogger(argc, argv))
+		return (1);
+
+	std::cout << yel("=======================================\n"
+					 "         Running all unit tests\n"
+					 "=======================================\n");
+	
+	int	totalFailures = 0;
+	int	totalTestSuites = 0;
+
+	totalTestSuites++;
+	totalFailures += testIntContainer();
+
+		std::cout << blu("Test Suites Summary\n")
+			  << wht("Total test suits: ") << totalTestSuites << '\n'
+			  << red("total failures: ") << totalFailures << '\n'
+			  << yel("=======================================\n");
+	if (totalFailures == 0)
+		std::cout << grn("ALL TESTS PASSED!\n");
+}
---
+++ cpp08/ex00/src/tests.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diade-so <diade-so@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 10:17:56 by diade-so          #+#    #+#             */
+/*   Updated: 2026/03/04 10:17:57 by diade-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tests.hpp"
+#include "Logger.hpp"
+#include <fstream>
+#include <string>
+
+
+void printHeader(const std::string& testName)
+{
+    std::cout << YEL << "Testing " << testName
+			  << RESET << '\n';
+}
+
+void printSummary(int failed, int total)
+{
+    std::cout << blu("Unit Test Summary\n")
+              << red("Tests failed: ") << failed << '\n'
+              << "Total tests: " << total << '\n';
+
+    if (failed == 0)
+	{
+        std::cout << grn("All tests passed!\n");
+	}
+	std::cout << yel("=======================================\n");
+}
